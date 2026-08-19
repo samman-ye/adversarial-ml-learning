2ae7620e5cc4c0e41f508bec2d2d6bcd45f3971d
samman <sammanshrestha140@gmail.com>
2026-08-19 06:22:11 +0000

    Intial commit: First Python program

---
+++ hello.py
@@ -0,0 +1,11 @@
+#first code
+#this is a comment
+name =input("Enter yo name")
+age =int(input("Enter yo age"))
+
+print(f"Hello {name}!")
+print(f"You are {age} years old")
+print(f"Next year you you will be {age + 1}")
+
+#maths
+print(f"2+2={2+2}")
