7bd1d48532bb927dcd6acb01a07bd027fdb5c841
samman <sammanshrestha140@gmail.com>
2026-08-20 14:16:03 +0000

    day-2

---
+++ variables.py
@@ -0,0 +1,19 @@
+#variables and datatypes
+name="samman shrestha"
+age=18
+height=5.7
+is_learning=True
+
+numbers=[1,2,3,4,5]
+model_config={"name": "Resnet50" , "accuracy":93.5}
+
+print(f"Your name is:{name}")
+print(f"type of age: {type(age)}")
+print(f"List: {numbers}")
+print(f"Model config: {model_config}")
+
+#loops
+numbers=[10,20,30,40,50]
+for num in numbers:
+    print(f"numbers are: {num}")
+
